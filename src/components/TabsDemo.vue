5ce2ae620456875dd7e22c1e87fc2ca8b4ee503c
cnqinglin <2207328000@qq.com>
2022-04-14 11:45:49 +0800

    开始tabs 组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Tabs 示例</div>
+  <Tabs>
+    <Tab title="导航一">内容一</Tab>
+    <Tab title="导航二">内容二</Tab>
+  </Tabs>
 </template>
+<script lang="ts">
+import Tabs from '../lib/tabs.vue';
+import Tab from '../lib/tab.vue';
+export default{
+  components:{
+    Tabs,
+    Tab
+  },
+  setup() {
+    
+  },
+}
+</script>
---
+++ src/lib/tabs.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>tabs示例12</div>
+</template>
+<script lang="ts">
+export default {
+
+}
+</script>
