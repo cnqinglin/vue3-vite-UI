a85d61f3ee6effa1978d1a4ae98e1d4d2fb17742
cnqinglin <2207328000@qq.com>
2022-04-21 08:30:49 +0800

    update

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export {default as Switch} from './Switch.vue';
+export {default as Buttton} from './Button.vue';
+export {default as Tabs} from './tabs.vue';
+export {default as Tab} from './tab.vue';
+export {default as Dialog} from './Dialog.vue';
+export {default as OpenDialog} from './openDialog.vue';
+export {default as Input} from './input.vue';
